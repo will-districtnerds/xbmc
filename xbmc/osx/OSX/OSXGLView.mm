cb94de12cb1070af719f2e79742e648ee5893bf8
S. Davilla <davilla@4pi.com>
2015-08-30 21:02:23 -0400

    fixed, random startup crash. you cannot update tracking areas here

--- xbmc/osx/OSX/OSXGLView.mm
+++ xbmc/osx/OSX/OSXGLView.mm
@@ -48,8 +48,6 @@
     m_glcontext = [[NSOpenGLContext alloc] initWithFormat:m_pixFmt shareContext:nil];
   }
   
-  [self updateTrackingAreas];
-  
   GLint swapInterval = 1;
   [m_glcontext setValues:&swapInterval forParameter:NSOpenGLCPSwapInterval];
   [m_glcontext makeCurrentContext];
