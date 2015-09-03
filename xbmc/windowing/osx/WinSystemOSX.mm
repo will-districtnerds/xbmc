79d05c2122babe46752f5b0f810735318ee8723d
S. Davilla <davilla@4pi.com>
2015-09-03 18:31:24 -0400

    fixed, 1) do not blindly hide the system mouse, 2) native fullscreen need a resize hint, 3) add pool, yes, Im paranoid

--- xbmc/windowing/osx/WinSystemOSX.mm
+++ xbmc/windowing/osx/WinSystemOSX.mm
@@ -553,7 +553,6 @@ bool CWinSystemOSX::CreateNewWindow(const std::string& name, bool fullScreen, RE
   NSPoint mouse = [NSEvent mouseLocation];
   if ([NSWindow windowNumberAtPoint:mouse belowWindowWithWindowNumber:0] == ((NSWindow *)m_appWindow).windowNumber)
   {
-    Cocoa_HideMouse();
     // warp XBMC cursor to our position
     NSPoint locationInWindowCoords = [(NSWindow *)m_appWindow mouseLocationOutsideOfEventStream];
     XBMC_Event newEvent;
@@ -828,7 +827,10 @@ bool CWinSystemOSX::SetFullScreen(bool fullScreen, RESOLUTION_INFO& res, bool bl
       [(NSWindow*)m_appWindow toggleFullScreen:nil];
     }
   }
-  
+
+  if (CDarwinUtils::DeviceHasNativeFullscreen())
+    ResizeWindow(m_nWidth, m_nHeight, -1, -1);
+
   return true;
 }
 
@@ -1018,6 +1020,8 @@ bool CWinSystemOSX::FlushBuffer(void)
 
 bool CWinSystemOSX::IsObscured(void)
 {
+  CCocoaAutoPool pool;
+
   if (m_bFullScreen && !CSettings::GetInstance().GetBool(CSettings::SETTING_VIDEOSCREEN_FAKEFULLSCREEN))
     return false;// in true fullscreen mode - we can't be obscured by anyone...
 
