2a3c967f9793a6a5a997b96a1af4b66a2dc8077b
Memphiz <memphis@machzwo.de>
2015-12-21 12:01:46 +0100

    [application] - fixed some stuff which is not suitable for darwin

--- xbmc/Application.cpp
+++ xbmc/Application.cpp
@@ -1108,7 +1108,9 @@ bool CApplication::Initialize()
     StringUtils::Format(g_localizeStrings.Get(178).c_str(), g_sysinfo.GetAppName().c_str()),
     "special://xbmc/media/icon256x256.png", EventLevel::Basic)));
 
+#if !defined(TARGET_DARWIN_IOS)
   g_peripherals.Initialise();
+#endif
 
   // Load curl so curl_global_init gets called before any service threads
   // are started. Unloading will have no effect as curl is never fully unloaded.
@@ -4529,8 +4531,9 @@ void CApplication::Process()
     m_slowTimer.Reset();
     ProcessSlow();
   }
-
+#if !defined(TARGET_DARWIN)
   g_cpuInfo.getUsedPercentage(); // must call it to recalculate pct values
+#endif
 }
 
 // We get called every 500ms
