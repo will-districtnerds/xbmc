99561389630064ba248b55c4e6e60c8044859b60
Memphiz <memphis@machzwo.de>
2016-02-06 14:31:52 +0100

    [VideoNav] - allow direct access to popup the videoinfo dialog for a given file item. This is needed for the tvos topshelf feature (pick a movie from the recently added topshelf will start kodi and popup the video info dialog)

--- xbmc/video/windows/GUIWindowVideoNav.cpp
+++ xbmc/video/windows/GUIWindowVideoNav.cpp
@@ -89,6 +89,12 @@ CGUIWindowVideoNav::~CGUIWindowVideoNav(void)
 {
 }
 
+CGUIWindowVideoNav &CGUIWindowVideoNav::GetInstance()
+{
+  static CGUIWindowVideoNav sWNav;
+  return sWNav;
+}
+
 bool CGUIWindowVideoNav::OnAction(const CAction &action)
 {
   if (action.GetID() == ACTION_TOGGLE_WATCHED)
@@ -1330,3 +1336,10 @@ bool CGUIWindowVideoNav::ApplyWatchedFilter(CFileItemList &items)
 
   return listchanged;
 }
+
+void CGUIWindowVideoNav::ShowVideoInfo(CFileItem &item)
+{
+  ADDON::ScraperPtr info;
+  CGUIWindowVideoNav::GetInstance().OnItemInfo(item, info);
+}
+
