7e2e97f1aad12ec489e8b1d8e06ca0717ada6bc9
Memphiz <memphis@machzwo.de>
2017-06-07 16:31:16 +0200

    [osx] - hookup the updater into osx main and add the new files to cmake

--- xbmc/platform/darwin/osx/SDLMain.mm
+++ xbmc/platform/darwin/osx/SDLMain.mm
@@ -19,6 +19,7 @@
 #import "PlatformDefs.h"
 #import "messaging/ApplicationMessenger.h"
 #import "storage/osx/DarwinStorageProvider.h"
+#import "utils/UpdateHandler.h"
 
 #import "platform/darwin/osx/HotKeyController.h"
 #import "platform/darwin/DarwinUtils.h"
@@ -67,7 +68,12 @@ static NSString *getApplicationName(void)
 
   return appName;
 }
-static void setupApplicationMenu(void)
+
+@interface XBMCApplication : NSApplication
+- (IBAction)checkForUpdates:(id)sender;
+@end
+
+static void setupApplicationMenu()
 {
   // warning: this code is very odd
   NSMenu *appleMenu;
@@ -93,6 +99,13 @@ static void setupApplicationMenu(void)
   [appleMenu addItemWithTitle:@"Show All" action:@selector(unhideAllApplications:) keyEquivalent:@""];
 
   [appleMenu addItem:[NSMenuItem separatorItem]];
+  
+  if (CDarwinUtils::IsOsxAppBundle())
+  {
+    menuItem = (NSMenuItem *)[appleMenu addItemWithTitle:@"Check for updates" action:@selector(checkForUpdates:) keyEquivalent:@""];
+    [menuItem setTarget:[XBMCApplication sharedApplication]];
+    [appleMenu addItem:[NSMenuItem separatorItem]];
+  }
 
   title = [@"Quit " stringByAppendingString:appName];
   [appleMenu addItemWithTitle:title action:@selector(terminate:) keyEquivalent:@"q"];
@@ -154,15 +167,17 @@ static void setupWindowMenu(void)
   [windowMenuItem release];
 }
 
-@interface XBMCApplication : NSApplication
-@end
-
 @implementation XBMCApplication
 
 // Called before the internal event loop has started running.
 - (void) finishLaunching
 {
   [super finishLaunching];
+}
+
+- (IBAction)checkForUpdates:(id)sender
+{
+  CUpdateHandler::GetInstance().CheckForUpdate();
 }
 
 // Invoked from the Quit menu item
@@ -505,7 +520,7 @@ int main(int argc, char *argv[])
 {
   NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
   XBMCDelegate *xbmc_delegate;
-
+  
   // Block SIGPIPE
   // SIGPIPE repeatably kills us, turn it off
   {
